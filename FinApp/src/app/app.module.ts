c879e8224c6f8dc0018488ce8f9efe944aea29fa
JuanS1lva <juan.silva.vane@gmail.com>
2020-10-08 12:12:08 -0500

    Se probo agregando componentes

--- FinApp/src/app/app.module.ts
+++ FinApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
