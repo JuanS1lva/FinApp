0ef888e2831985dbc30145b205c7fbaa5726ad7f
EliasPretelt <66378392+EliasPretelt@users.noreply.github.com>
2020-10-09 06:16:17 -0500

    footer

--- FinApp/src/app/app.module.ts
+++ FinApp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
