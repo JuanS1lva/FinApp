3ae44d16f38748dc91136c3e44f7729f5cd282d7
EliasPretelt <66378392+EliasPretelt@users.noreply.github.com>
2020-10-10 10:50:18 -0500

    ajustes en imagenes en banner

--- FinApp/src/app/app.module.ts
+++ FinApp/src/app/app.module.ts
@@ -6,11 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-<<<<<<< HEAD
 import { FooterComponent } from './footer/footer.component';
-=======
 import { BannerComponent } from './banner/banner.component';
->>>>>>> 56d02d3a3eadbc0e6329e3326568d068dcf03c6d
 
 @NgModule({
   declarations: [
@@ -18,11 +15,8 @@ import { BannerComponent } from './banner/banner.component';
     HeaderComponent,
     HomeComponent,
     NavbarComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
+    FooterComponent,
     BannerComponent
->>>>>>> 56d02d3a3eadbc0e6329e3326568d068dcf03c6d
   ],
   imports: [
     BrowserModule,
